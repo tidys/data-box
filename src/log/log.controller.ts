2c2116fd044b61a51680175f958f6bc05100b7fb
xu_yanfeng <xu_yanfeng@126.com>
2023-11-22 15:43:34 +0800

    连表查询

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -12,6 +12,17 @@ import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 import { User } from './decorators';
 import { LogData } from './data';
 
+class AddLog {
+  @IsNotEmpty()
+  @IsString()
+  str: string;
+}
+class GetLog {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+}
+
 @Controller('log')
 export class LogController {
   constructor(private readonly logService: LogService) {}
@@ -26,5 +37,13 @@ export class LogController {
   @Post('post-test-user')
   testPostUser(@User() user: string) {
     return user;
+  }
+  @Post('add-log')
+  addLog(@Body() obj: AddLog) {
+    return this.logService.addLog(obj.str);
+  }
+  @Post('get-log')
+  getLog(@Body() obj: GetLog) {
+    return this.logService.getLog(obj.id);
   }
 }
--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, FindManyOptions } from 'typeorm';
 import { LogData } from './data';
+import { noop } from 'rxjs';
 @Injectable()
 export class LogService {
   private readonly logger = new Logger(LogService.name);
@@ -15,5 +16,15 @@ export class LogService {
   testPost(str: string = '') {
     this.logger.log(`test-post: ${str}`);
     return 'test-post: ' + str;
+  }
+  async addLog(str: string): Promise<LogData> {
+    const result = this.repository.create({ name: str });
+    await this.repository.save(result);
+    return result;
+  }
+  async getLog(id: number) {
+    this.logger.log(`get log by id: ${id}`);
+    const result = await this.repository.find({ where: { id: id } });
+    return result ?? 'not found';
   }
 }

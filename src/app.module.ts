dc60e626b20a107001a6e88414f76c02fef9af62
xu_yanfeng <xu_yanfeng@126.com>
2023-11-22 16:33:10 +0800

    postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogController } from './log/log.controller';
@@ -6,20 +6,34 @@ import { LogService } from './log/log.service';
 import { LogModule } from './log/log.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+let db: DynamicModule;
+if (true) {
+  db = TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'dpg-ckdqr4kgonuc738of11g-a.oregon-postgres.render.com',
+    password: 'G8cLdy0ZpQZitJwrbJ7OtBZkGNRkFWIV',
+    port: 5432,
+    username: 'user_3ria_user',
+    database: 'user_3ria',
+    autoLoadEntities: true,
+    ssl: true,
+    synchronize: true,
+  });
+} else {
+  db = TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '00000000',
+    database: 'log',
+    autoLoadEntities: true,
+    synchronize: true, // 数据库自动同步 entity 文件修改
+  });
+}
+
 @Module({
-  imports: [
-    LogModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '00000000',
-      database: 'log',
-      autoLoadEntities: true,
-      synchronize: true, // 数据库自动同步 entity 文件修改
-    }),
-  ],
+  imports: [LogModule, db],
   controllers: [AppController],
   providers: [AppService],
 })
